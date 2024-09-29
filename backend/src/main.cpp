3f577358bf8f976cc1dae9248be183ddc685b4ea
Anton <119325773+impelixx@users.noreply.github.com>
2024-09-29 22:41:06 +0300

    fix: main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -16,6 +16,8 @@ int main() {
     res.set_header("Access-Control-Allow-Headers", "*");
   });
 
+
+  std::cout << "Starting server on port 8080" << std::endl;
   app.listen("0.0.0.0", 8080);
 
   return 0;
