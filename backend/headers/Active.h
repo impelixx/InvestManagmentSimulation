b3f21bd60be5e908e459d9c4626bdd354e86b70b
Jkey189 <olegator5496@gmail.com>
2024-09-29 14:11:28 +0300

    The "headers" folder has been created, where the basis of the Active superclass is implemented The constructor, destructor and setters are implemented in the file Action.cpp The "include.h" file has been added with all the necessary C++ libraries

---
+++ backend/headers/Active.h
@@ -0,0 +1,35 @@
+#ifndef ACTIVE_H
+#define ACTIVE_H
+
+
+#include "../include/includes.h"
+
+
+class Active {
+public:
+	Active() : name_(""), income_(std::vector<int>()), amount_(0.0), price_(0.0) {}
+	Active(std::string& name, std::vector<int>& income, double amount, double price);
+	~Active();
+
+	// sets
+	void setIncomeGraph	(std::vector<int>& income);
+	void setName				(std::string& name);
+	void setAmount			(double amount);
+	void setPrice				(double price);
+
+	// get
+	std::vector<int> 	getIncomeGraph() 	const { return income_; }
+	std::string 			getName() 				const { return name_; 	}
+	double 						getAmount() 			const { return amount_; }
+	double 						getPrice() 				const { return price_; 	}
+	
+	
+private:
+	std::string 			name_;
+	std::vector<int> 	income_;
+	double 						amount_;
+	double 						price_;
+};
+
+
+#endif // ACTIVE_H
---
+++ backend/src/Active.cpp
@@ -0,0 +1,61 @@
+#include "../headers/Active.h"
+
+
+// constructor / destructor
+Active::Active(std::string& name, std::vector<int>& income, double amount, double price) {
+  if (name.empty()) { // will better to rewrite this to try/catch
+    throw std::length_error("No any name!");
+    return;
+  }
+  if (income.empty()) {
+    throw std::length_error("Income graph is empty!");
+    return;
+  }
+
+  name_ = name;
+  amount_ = amount;
+  price_ = price;
+  income_.reserve(income.capacity());
+  for (auto el : income) {
+    income_.push_back(el);
+  }
+}
+
+Active::~Active() {
+  name_ 	= "";
+  amount_ = 0.0;
+  price_ 	= 0.0;
+  
+  income_.clear();
+}
+
+
+// sets
+void Active::setIncomeGraph(std::vector<int>& income) {
+  if (income.empty()) {
+    throw std::length_error("Income graph is empty!");
+    return;
+  }
+
+  income_.reserve(income.capacity());
+  for (auto el : income) {
+    income_.push_back(el);
+  }
+}
+
+void Active::setName(std::string& name) { 
+  if (name.empty()) {
+    throw std::length_error("No any name!");
+    return;
+  }
+
+  name_ = name;
+}
+
+void Active::setAmount(double amount) {
+  amount_ = amount;
+}
+
+void Active::setPrice(double price) {
+  price_ = price;
+}
