868c63c7cfd9abe68f2fe19e8e42a2763c75ecb5
Anton <119325773+impelixx@users.noreply.github.com>
2024-09-29 12:42:07 +0300

    rewrite: main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -16,22 +16,7 @@ int main() {
     res.set_header("Access-Control-Allow-Headers", "*");
   });
 
-  app.Get("/ping", [](const auto& req, auto& res) {
-    json response = {
-      {"ok", true}
-    };
-
-    JSON_RESPONSE(response);
-  });
-
-  app.Post("/increment", [](const auto& req, auto& res) {
-    json response = {
-      {"ok", true},
-      {"counter", ++counter}
-    };
-
-    JSON_RESPONSE(response);
-  });
+  
 
   app.listen("0.0.0.0", 8080);
 
