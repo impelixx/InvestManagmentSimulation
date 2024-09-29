50366090db207c8363082e656d875e169b5cfd75
Anton <119325773+impelixx@users.noreply.github.com>
2024-09-29 13:31:18 +0300

    hello world setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './index.module.scss'
-import { HelloPage } from './pages/Hello'
-import axios from 'axios';
-
-
-axios.defaults.baseURL = process.env.NODE_ENV === "local" ? "http://localhost:8080" : "https://spotika4.difhel.dev";;
-
-// beb
+import './style.css'
+import { HelloPage } from './pages/Hello/HelloPage.tsx'
+// import axios from 'axios';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
