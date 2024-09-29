36533b1fc88ebc715f94697aa0767790834c7c8c
Anton <119325773+impelixx@users.noreply.github.com>
2024-09-29 22:33:54 +0300

    add: tradingview widget

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import TradingViewWidget from './components/TradingViewWidget.tsx'
 
 import './style.css'
 import { HelloPage } from './pages/Hello/HelloPage.tsx'
 // import axios from 'axios';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HelloPage />
-  </StrictMode>
+	<div className='h-100 w-700'>
+		<TradingViewWidget />
+	</div>
 )
