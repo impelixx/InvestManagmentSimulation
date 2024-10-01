bc9260c5a91cdf084c89acbb26b66976b9843399
Jkey189 <olegator5496@gmail.com>
2024-10-01 17:36:38 +0300

    Create currency.h file / without .cpp

---
+++ backend/headers/currency.h
@@ -0,0 +1,19 @@
+#ifndef CURRENCY_H
+#define CURRENCY_H
+
+
+#include "../include/includes.h"
+#include "active.h"
+
+
+class Currency : public Active {
+public:
+  
+
+
+private:
+
+};
+
+
+#endif // CURRENCY_H
