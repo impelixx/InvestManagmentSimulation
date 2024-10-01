16fd9273f7f5dd44c3909aeda0309a782a6d0740
repa <166624765+reprenter@users.noreply.github.com>
2024-10-01 17:27:59 +0300

    rewrite: main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -10,13 +10,12 @@ int counter = 0;
 
 int main() {  
   httplib::Server app;
-
   app.set_post_routing_handler([](const auto& req, auto& res) {
     res.set_header("Access-Control-Allow-Origin", "*");
     res.set_header("Access-Control-Allow-Headers", "*");
   });
 
-  // sosat amerika
+  // ne sosat amerika
 
   app.listen("0.0.0.0", 8080);
 
