86c7fff62e66f2f9e158719284a275dcd1fa1f15
repa <166624765+reprenter@users.noreply.github.com>
2024-10-08 10:48:02 +0300

    cleaned main

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -10,18 +10,6 @@
 
 int counter = 0;
 int main() {
-    Active act;
-    act.setName("Apple");
-    act.setPrice(2000);
-    act.setAmount(100);
-    act.setRisk(0.1);
-
-    while (true) {
-        std::cout << act.getPrice() << std::endl;
-        act.changePrice();
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-
   httplib::Server app;
 
   app.set_post_routing_handler([](const auto& req, auto& res) {
